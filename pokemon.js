9d55a14167ea9a27df48dd4b47031b597ae5fb68
Jayden Andrews <jaydenrandrews@gmail.com>
2023-12-09 18:11:32 -0500

    Updates v2

--- pokemon.js
+++ pokemon.js
@@ -25,6 +25,20 @@ app.get("/apply", (request, response) => {
     response.render("application");
 });
 
+app.post("/apply", async (request, response) => {
+  let {name, email, type, pokemon} = request.body;
+  try {
+      await client.connect();
+      let variables = {name: name, email: email, type: type, pokemon: pokemon};
+      await insertApplication(client, databaseAndCollection, individual);
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+  response.render("confirmation", variables);
+})
+
 app.get("/displayType", (request, response) => {
   response.render("dsplayType");
 });
@@ -55,22 +69,8 @@ app.post("/displayType", async (request, response) => {
   const variables = {
       chart: table
   }
-  response.render("gpaDisplay", variables);
+  response.render("typeChart", variables);
 });
-
-app.post("/apply", async (request, response) => {
-    let {name, email, type, pokemon} = request.body;
-    try {
-        await client.connect();
-        let variables = {name: name, email: email, type: type, pokemon: pokemon};
-        await insertApplication(client, databaseAndCollection, individual);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-    response.render("confirmation", variables);
-})
 
 // Add more middleware functions here
 
