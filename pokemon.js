47dd5e72127720ccdba771546e1259079b340275
Kai Walter <112970614+Kaiewalter@users.noreply.github.com>
2023-12-15 17:47:54 -0500

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -26,8 +26,8 @@ app.get("/apply", (request, response) => {
     response.render("application");
 });
 app.post("/apply", async (request, response) => {
-  let {name, email, type} = request.body;
-  let variables = {name: name, email: email, type: type};
+  let {name, email, type, pokemon} = request.body;
+  let variables = {name: name, email: email, type: type, favorite: pokemon};
   await insertApplication(variables);
   response.render("confirmation", variables);
 })
@@ -48,25 +48,37 @@ app.get("/displayType", (request, response) => {
   response.render("displayType");
 });
 app.post("/displayType", async (request, response) => {
-  const type = request.body.type;
-  const additionalRows = await findType(type);
-
-  let table = "<table border='1'><thead><tr><th>Name</th><th>Email</th></tr></thead><tbody></tbody>";
+  const {type, pokemon, filter} = request.body;
+  let additionalRows = "";
+  additionalRows = await find(type, pokemon, filter);
+  let table = "<table border='1'><thead><tr><th>Name</th><th>Email</th><th>Favorite Type</th><th>Favorite Pokemon</th></tr></thead><tbody>";
   table += additionalRows;
   table += "</tbody></table><br>";
-  const variables = {chart: table};
+  const variables = {chart: table, filter: filter.toLowerCase()};
   response.render("typeChart", variables);
 });
-async function findType(type) {
+async function find(type, pokemon, filter) {
   let additionalRows = "";
   try {
     await client.connect();
     const cursor = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).find({});
     const result = await cursor.toArray();
     for (let elem of result) {
-      if (elem.type === type) {
+      if (filter === "Type" && elem.type === type) {
         additionalRows += `<tr><td>${elem['name']}</td>`;
-        additionalRows += `<td>${elem['email']}</td></tr>`;        
+        additionalRows += `<td>${elem['email']}</td>`;
+        additionalRows += `<td>${elem['type']}</td>`;
+        additionalRows += `<td>${elem['favorite']}</td></tr>`;
+      } else if (filter === "Pokemon" && elem.favorite === pokemon) {
+        additionalRows += `<tr><td>${elem['name']}</td>`;
+        additionalRows += `<td>${elem['email']}</td>`;
+        additionalRows += `<td>${elem['type']}</td>`;
+        additionalRows += `<td>${elem['favorite']}</td></tr>`;
+      } else if (filter === "Both" && elem.type === type && elem.favorite === pokemon) {
+        additionalRows += `<tr><td>${elem['name']}</td>`;
+        additionalRows += `<td>${elem['email']}</td>`;
+        additionalRows += `<td>${elem['type']}</td>`;
+        additionalRows += `<td>${elem['favorite']}</td></tr>`;
       }
     }
   } catch (e) {
@@ -84,13 +96,22 @@ app.get("/displayFavorite", (request, response) => {
 })
 app.post("/displayFavorite", async (request, response) => {
   let pokemon = request.body.favorite;
+  if (pokemon === "") {
+    let email = request.body.email;
+    pokemon = await findFavorite(email);
+  }
+
   let formattedPokemon = pokemon.toLowerCase()
   let url = `https://pokeapi.co/api/v2/pokemon/${formattedPokemon}/`;
   let info = "";
   let result = await fetch(url);
   if (result.status === 404) {
-    info += "Pokemon not found, please try again with a valid pokemon!";
+    info += "Pokemon not found, please try again with a valid pokemon!<br>";
     const variables = {pokemon: pokemon, info: info};
+    response.render("pokemonInfo", variables);
+  } else if (pokemon === "") {
+    info += "Either invalid email provided or pokemon not provided<br>";
+    const variables = {pokemon: "*POKEMON NOT FOUND*", info: info};
     response.render("pokemonInfo", variables);
   } else {
     let json = await result.json();
@@ -104,7 +125,23 @@ app.post("/displayFavorite", async (request, response) => {
     response.render("pokemonInfo", variables);
   }
 })
-
-
+async function findFavorite(email) {
+  let favorite = "";
+  try {
+    await client.connect();
+    const cursor = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).find({});
+    const result = await cursor.toArray();
+    for (let elem of result) {
+      if (elem.email === email) {
+        favorite = elem.favorite;
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+    return favorite;
+  }
+}
 
 app.listen(portNumber);
